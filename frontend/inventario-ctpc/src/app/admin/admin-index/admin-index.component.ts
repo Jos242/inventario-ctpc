a6b03f6eaa60e72e9d95e64c179db2dd5001656c
Jos242 <73893743+Jos242@users.noreply.github.com>
2024-06-18 08:54:55 -0600

    actas mas que todo

---
+++ frontend/inventario-ctpc/src/app/admin/admin-index/admin-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-index.component.html',
+  styleUrl: './admin-index.component.scss'
+})
+export class AdminIndexComponent {
+
+}
