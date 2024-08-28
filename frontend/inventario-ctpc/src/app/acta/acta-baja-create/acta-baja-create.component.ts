74465a40424441397e2ef606e013ec5b8322aad1
Andres Hernandez <ahernandez20910@gmail.com>
2024-08-28 14:38:34 -0600

    xd

--- frontend/inventario-ctpc/src/app/acta/acta-baja-create/acta-baja-create.component.ts
+++ frontend/inventario-ctpc/src/app/acta/acta-baja-create/acta-baja-create.component.ts
@@ -188,7 +188,7 @@ export class ActaBajaCreateComponent {
     
     const tableHeightLimitPx = (contentheightPx * (this.currentRow == 0 || this.currentRow == this.activoTablas.length - 1 ? 0.51 : 0.75)).toFixed(2);
     
-    if (currentPx > (parseFloat(tableHeightLimitPx) * 0.65) && currentPx < tableHeightLimitPx) {
+    if (currentPx + headerSize > (parseFloat(tableHeightLimitPx) * 0.65) && currentPx + headerSize < tableHeightLimitPx) {
       const heightToSetPx = (contentheightPx - ((this.currentRow == 0 ? this.header.nativeElement.offsetHeight : 99.86  ) + (this.currentHeightPx + 1 + headerSize))).toFixed(2);
 
       const blanco = this.blancos.toArray()[this.currentRow]?.nativeElement;
