ad48f77f499f0b360b13783168a549e1465be40d
Andres Hernandez <ahernandez20910@gmail.com>
2025-11-13 02:48:49 -0600

    x

--- frontend/inventario-ctpc/src/environments/environment.ts
+++ frontend/inventario-ctpc/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiURL: 'http://127.0.0.1:8000/'
+    //apiURL: 'http://127.0.0.1:8000/'
     // apiURL: 'http://django:8001/'
     // apiURL: 'http://181.193.121.122/apinventario/'
-    // apiURL: '/inventario/api/'
+     apiURL: '/inventario/api/'
 };
